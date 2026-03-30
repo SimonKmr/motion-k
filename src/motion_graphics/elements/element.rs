29e6c14816dfdc304d7a0c642867cb913df575b0
SimonKmr <41740705+SimonKmr@users.noreply.github.com>
2026-03-30 13:21:44 +0200

    add DrawInfo gives the draw method more context to work with

--- src/motion_graphics/elements/element.rs
+++ src/motion_graphics/elements/element.rs
@@ -2,9 +2,14 @@ use vector2d::Vector2D;
 use crate::motion_graphics::attributes::static_attribute::StaticAttribute;
 
 pub trait Element {
-    fn draw_on(&self, frame:usize, canvas: &skia_safe::canvas::Canvas) -> Result<(),&'static str>;
+    fn draw_on(&self, frame:usize, canvas: &skia_safe::canvas::Canvas, draw_info: &DrawInfo) -> Result<(),&'static str>;
     fn boxed(self) -> Box<dyn Element>
     where Self: Sized + 'static,{
         Box::new(self)
     }
+}
+
+pub struct DrawInfo{
+    pub width:f32,
+    pub height:f32,
 }
